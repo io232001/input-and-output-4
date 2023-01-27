b52c13ba9c350da747aa9d68d50394fc1e2b539a
iosbd <io232001@gmail.com>
2023-01-27 14:34:26 +0100

    Initial commit

---
+++ src/it/develhope/io/PrintYourNameAndPi.java
@@ -0,0 +1,13 @@
+package it.develhope.io;
+
+public class PrintYourNameAndPi
+{
+    public static void main (String arg[])
+    {
+        String name= "ionut";
+        double pi;
+        pi=3.141590;
+        System.out.printf("%s", name.toUpperCase()+"\n");
+        System.out.printf("the numbers is %f ",pi);
+    }
+}
